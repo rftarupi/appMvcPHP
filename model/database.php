ef6c24ce66f404053ed0adf9e628a081a5b29000
rftarupi <rick030gp@gmail.com>
2018-05-10 08:36:55 -0500

    Cambio cadena de conexion

--- model/database.php
+++ model/database.php
@@ -3,7 +3,8 @@ class Database
 {
     public static function Conectar()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=bdd;charset=utf8', 'user', 'pass');
+
+        $pdo = new PDO('mysql:host=appmvcservermysql.mysql.database.azure.com;dbname=appmvctarupi;charset=utf8', 'rtarupi@appmvcservermysql', 'Rtc10lovely1');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
